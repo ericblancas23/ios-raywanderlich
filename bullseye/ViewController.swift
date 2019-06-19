df7fefc35f23171855e4e2064eef71b3cb065a6f
Eric Blancas <ericblancas@erics-mbp.attlocal.net>
2019-06-18 21:06:52 -0700

    first project

---
+++ bullseye/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  bullseye
+//
+//  Created by Eric Blancas on 6/17/19.
+//  Copyright © 2019 Eric Blancas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World", message: "This is my first app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+

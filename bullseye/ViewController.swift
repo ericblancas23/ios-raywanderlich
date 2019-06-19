d4c43be1a2888a890847b302155dbb6289c0bdf3
Eric Blancas <ericblancas@erics-mbp.attlocal.net>
2019-06-18 22:36:37 -0700

    messing around with swift collection types

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -10,26 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 0;
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
     }
-
+    
     @IBAction func showAlert() {
-        let message = "The Value of \(currentValue)"
-        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        let message = "The value of the slider is now: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The Value of \(slider.value)")
         let roundedValue = slider.value.rounded()
-        print("The Rounded Value of \(roundedValue)")
         currentValue = Int(roundedValue)
     }
 }

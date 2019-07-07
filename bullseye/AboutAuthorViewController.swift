226b4b421ca6351a8562b949743bfc31cef026cc
Eric Blancas <ericblancas23@gmail.com>
2019-07-07 08:13:19 -0700

    added new modal

---
+++ bullseye/AboutAuthorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutAuthorViewController.swift
+//  bullseye
+//
+//  Created by Eric Blancas on 7/7/19.
+//  Copyright © 2019 Eric Blancas. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
